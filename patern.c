b7af7c137633d0a184e0ec85ac2db61a9904247f
priyaadodariya1008-droid <priya.adodariya1008@gmail.com>
2025-09-24 19:00:34 +0530

    Add files via upload

---
+++ patern.c
@@ -0,0 +1,155 @@
+#include<stdio.h>
+int main(){
+    for(char i='A';i<='E';i++){
+        for(char j='A';j<=i;j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+}
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(char j=1;j<=i;j++){
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(char i='A';i<='E';i++){
+//         for(char j='A';j<=i;j++){
+//             printf("%c",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("%d",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=5;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(char i='E';i>='A';i--){
+//         for(char j='A';j<=i;j++){
+//             printf("%c",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=5;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             printf("%d",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(char i='E';i>='A';i--){
+//         for(char j='A';j<=i;j++){
+//             printf("%c",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=5;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// #include <stdio.h>
+// int main() {
+//     for(int i=1;i<=5;i++) {
+//         for(int k=i;k<5;k++) {
+//             printf(" ");
+//         }
+//         for(int j=1;j<=i;j++) {
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+   
+// #include <stdio.h>
+// int main() {
+//     for(int i=1;i<=5;i++) {
+//         for(int k=i;k<5;k++) {
+//             printf(" ");
+//         }
+//         for(int j=1;j<=i;j++) {
+//             printf("%d",i);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main() {
+//     for(char i='A';i<='E';i++) {
+//         for(char k=i;k<'E';k++) {
+//             printf(" ");
+//         }
+//         for(char j='A';j<=i;j++) {
+//             printf("%c",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main() {
+//     for(char i='A';i<='E';i++) {
+//         for(char k=i;k<'E';k++) {
+//             printf(" ");
+//         }
+//         for(char j='A';j<=i;j++) {
+//             printf("%c",i);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
